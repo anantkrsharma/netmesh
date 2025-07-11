ee9b2244b0f089557ee3672dbc2c2ba82d29e2df
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-07-11 23:24:28 +0530

    bug: build errors fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from "next/image";
 import { ShieldCheck, Network, Users, Server, ArrowRight, Download, Github } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
